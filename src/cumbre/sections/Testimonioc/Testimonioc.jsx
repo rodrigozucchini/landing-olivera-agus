487f58e9c3d97123ee070dd4c815e2eed1ffb6ba
unknown <rodrigozucchini1997@gmail.com>
2023-10-01 22:52:08 -0300

    cambios produccion de imagenes

--- src/cumbre/sections/Testimonioc/Testimonioc.jsx
+++ src/cumbre/sections/Testimonioc/Testimonioc.jsx
@@ -15,11 +15,11 @@ const TestimonioC = () => {
             </div>
             <div className='cardc-testimonio'>
                 <img className="imagec-testimonio" src={Testimonio2} />
-                <p className='pc-testimonio'>“He podido pasar de 1 <br/>cliente al mes, a cerrar 3 en 48 hs”</p>
+                <p className='pc-testimonio'>“Con Flor logramos <br/>sacar nuestro rango consultor”</p>
             </div>
             <div className='cardc-testimonio'>
                 <img className="imagec-testimonio" src={Testimonio3} />
-                <p className='pc-testimonio'>“Con Flor logramos <br/>sacar nuestro rango consultor”</p>
+                <p className='pc-testimonio'>“He podido pasar de 1 <br/>cliente al mes, a cerrar 3 en 48 hs”</p>
             </div>
         </div>
     </div>
