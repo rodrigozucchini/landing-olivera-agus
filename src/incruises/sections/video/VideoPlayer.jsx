4b9b1b324bbeac1fc9083c480c15905189056b21
unknown <rodrigozucchini1997@gmail.com>
2023-10-12 22:58:02 -0300

    cambios botton

--- src/incruises/sections/video/VideoPlayer.jsx
+++ src/incruises/sections/video/VideoPlayer.jsx
@@ -1,14 +1,9 @@
-import React, { useState} from 'react';
+import React from 'react';
 import './VideoPlayer.css';
 import ReactPlayer from 'react-player';
 
 const VideoPlayer = () => {
   const videoUrl = 'https://vimeo.com/868543913?share=copy';
-  const [isPlayingg, setIsPlayingg] = useState(false);
-
-  const handlePlayPause = () => {
-    setIsPlayingg(!isPlayingg);
-  };
 
   return (
     <div className="container-position">
@@ -16,23 +11,11 @@ const VideoPlayer = () => {
     <ReactPlayer
           width="1000px"
           height="500px"
-          playing={isPlayingg}
           className='video-player-video'
-          controls={false} // Deshabilita todos los controles predeterminados
+          controls={true} // Deshabilita todos los controles predeterminados
           url={videoUrl}
         />
-        {isPlayingg ? 
-        (        
-        <button className="button-play-no" onClick={handlePlayPause}>
-        Pausa
-        </button>
-        ) 
-        : 
-        (        
-        <button className="button-play-yes" onClick={handlePlayPause}>
-        Reproducir
-        </button>)
-        }
+
     </div>
     </div>
   );
